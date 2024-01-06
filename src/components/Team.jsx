51612b72f50a4430376200922ac0c41ab480ae3c
Ayush Kumar <113000238+Redluigi1@users.noreply.github.com>
2024-01-06 11:40:44 +0530

    stuff

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import Zoom from 'react-reveal/Zoom';
+import React, { useEffect, useRef, useState } from "react";
 
 export const Team = (props) => {
+  const [inView, setInView] = useState(false);
+  const galleryRef = useRef(null);
+
+  const handleScroll = () => {
+    const element = galleryRef.current;
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      // Adjust the values below to make the inView window smaller
+      setInView(rect.top <= window.innerHeight * 0.8 && rect.bottom >= window.innerHeight * 0.7);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div id="team" className="text-center">
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Meet the Team</h2>
+          <h2>
+            <Zoom cascade when={inView}>
+              Meet the Team
+            </Zoom>
+          </h2>
           <p></p>
         </div>
-        <div className="team-scroll-container">
+        <div className="team-scroll-container" ref={galleryRef}>
           {props.data ? (
             <div id="row" className="team-scroll-row">
               {props.data.map((d, i) => (
@@ -19,19 +43,18 @@ export const Team = (props) => {
                   <div className="thumbnail">
                     {" "}
                     <img src={d.img} alt="..." className="team-img" />
-                    <div class="ag-courses_box">
-    <div class="ag-courses_item">
-      <a href="#" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
+                    <div className="ag-courses_box">
+                      <div className="ag-courses_item">
+                        <a href="#" className="ag-courses-item_link">
+                          <div className="ag-courses-item_bg"></div>
 
-        <div class="ag-courses-item_title">
-          <p>{d.name}</p>
-          <p>{d.job}</p>
-
-        </div>
-      </a>
-    </div>
-</div>
+                          <div className="ag-courses-item_title">
+                            <p>{d.name}</p>
+                            <p>{d.job}</p>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               ))}
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,27 +1,47 @@
 import { Image } from "./image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Zoom from 'react-reveal/Zoom';
 
 export const Gallery = (props) => {
+  const [inView, setInView] = useState(false);
+  const galleryRef = useRef(null);
+
+  const handleScroll = () => {
+    const element = galleryRef.current;
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      setInView(rect.top <= window.innerHeight && rect.bottom >= 0);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Upcoming/Past Events</h2>
-          
+          <h2>
+            <Zoom cascade when={!inView}>
+              Upcoming/Past Events
+            </Zoom>
+          </h2>
         </div>
         <div className="row">
           <div className="portfolio-items">
-    <div>
-          <a
-                  href="#about"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  View more events
-          </a>
-
-    </div>
-    <div><p></p></div>
-  
+            <div>
+              <a
+                href="#about"
+                className="btn btn-custom btn-lg page-scroll"
+              >
+                View more events
+              </a>
+            </div>
+            <div><p></p></div>
 
             {props.data
               ? props.data.map((d, i) => (
@@ -29,11 +49,12 @@ export const Gallery = (props) => {
                     key={`${d.title}-${i}`}
                     className="col-sm-6 col-md-4 col-lg-4"
                   >
-                    <Image
-                      title={d.title}
-                      Image={d.Image}
-                      
-                    />
+                    <div ref={galleryRef}>
+                      <Image
+                        title={d.title}
+                        Image={d.Image}
+                      />
+                    </div>
                   </div>
                 ))
               : "Loading..."}
